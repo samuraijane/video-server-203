ca46917931bf8fa68f6f05c9658afebb5db5be0d
Matthew Day <matt@matthewday.net>
2023-04-17 11:10:42 -0400

    add example for `res.send`

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.get('/anna', (req, res) => {
+  res.send('ANNA with `res.send`');
+});
+
 server.listen(8080, () => {
   console.log('The server is listening at PORT 8080');
 });
