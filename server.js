ff1fc098df5925ab94a953756b71c8004f6c4714
Matthew Day <matt@matthewday.net>
2023-04-17 11:11:46 -0400

    add example for `res.json`

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ server.get('/anna', (req, res) => {
   res.send('ANNA with `res.send`');
 });
 
+server.get('/billy', (req, res) => {
+  res.json({
+    message: 'BILLY with `res.json`'
+  });
+});
+
 server.listen(8080, () => {
   console.log('The server is listening at PORT 8080');
 });
