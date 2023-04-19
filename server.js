52fbce60243e0ec68c1f4fe2b3d8fbaa1a6a9089
Matthew Day <matt@matthewday.net>
2023-04-19 17:32:31 -0400

    reset code to a fresh starting point

--- server.js
+++ server.js
@@ -1,33 +1,9 @@
 const express = require('express');
-const { prepFileToSend } = require('./middleware');
 
 const server = express();
 
-// server.use(prepFileToSend);
-
-server.get('/anna', (req, res) => {
-  res.send('ANNA with `res.send`');
-});
-
-server.get('/billy', (req, res) => {
-  res.json({
-    message: 'BILLY with `res.json`'
-  });
-});
-
-
-server.get('/carlos', (req, res) => {
-  res.sendFile('./files/carlos.txt', {root: __dirname})
-});
-
-// old way
-// server.get('/david', (req, res) => {
-//   res.sendFile('./files/david.html', {root: __dirname});
-// });
-
-// new way
-server.get('/david', prepFileToSend, (req, res) => {
-  res.fileToShow('david');
+server.get('/heartbeat', (req, res) => {
+  res.json({"is": "working"});
 });
 
 server.listen(8080, () => {
